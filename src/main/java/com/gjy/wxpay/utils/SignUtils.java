37325a51d22ece59ebe734c26fa62b734e252231
哈哈哈哈哈18194085604 <484764968@qq.com>
2024-10-05 12:42:34 +0800

    feat:(分析和验证支付接口，写测试了测试类)

---
+++ src/main/java/com/gjy/wxpay/utils/SignUtils.java
@@ -0,0 +1,58 @@
+package com.gjy.wxpay.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * 签名工具类
+ */
+public class SignUtils {
+
+    public static String createSign(Map < String, String > params, String partnerKey) {
+        // 生成签名前先去除sign
+        params.remove("sign");
+        String stringA = packageSign(params, false);
+        String stringSignTemp = stringA + "&key=" + partnerKey;
+        return DigestUtils.md5Hex(stringSignTemp).toUpperCase();
+    }
+
+    public static String packageSign(Map< String, String > params, boolean urlEncoder) {
+        // 先将参数以其参数名的字典序升序进行排序
+        TreeMap< String, String > sortedParams = new TreeMap < String, String > (params);
+        // 遍历排序后的字典，将所有参数按"key=value"格式拼接在一起
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (Map.Entry< String, String > param: sortedParams.entrySet()) {
+            String value = param.getValue();
+            if (StringUtils.isBlank(value)) {
+                continue;
+            }
+            if (first) {
+                first = false;
+            } else {
+                sb.append("&");
+            }
+            sb.append(param.getKey()).append("=");
+            if (urlEncoder) {
+                try {
+                    value = urlEncode(value);
+                } catch (UnsupportedEncodingException e) {}
+            }
+            sb.append(value);
+        }
+        return sb.toString();
+    }
+
+    public static String urlEncode(String src) throws UnsupportedEncodingException {
+        return URLEncoder.encode(src, UTF_8.name()).replace("+", "%20");
+    }
+
+
+}
--- src/test/java/com/gjy/wxpay/WxPayApplicationTests.java
+++ src/test/java/com/gjy/wxpay/WxPayApplicationTests.java
@@ -1,13 +1,27 @@
 package com.gjy.wxpay;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.gjy.wxpay.utils.SignUtils;
 
-@SpringBootTest
-class WxPayApplicationTests {
+import java.util.HashMap;
+import java.util.Map;
 
-    @Test
-    void contextLoads() {
+public class WxPayApplicationTests {
+
+    public static void main(String[] args) {
+
+        long timestamp = System.currentTimeMillis() / 1000;
+        System.out.println(timestamp);
+
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put("mch_id", "1673424392");
+        dataMap.put("out_trade_no", "xfg240413001");
+        dataMap.put("total_fee", "0.01");
+        dataMap.put("body", "QQ公仔");
+        dataMap.put("timestamp", String.valueOf(timestamp));
+        dataMap.put("notify_url", "https://www.baidu.com");
+
+        System.out.println(SignUtils.createSign(dataMap, "6d3e889f359fcb83d150e9553a9217b9"));
+
     }
 
 }
