1767fc36734e930b2f78db40b3dcbbe5de8a9292
哈哈哈哈哈18194085604 <484764968@qq.com>
2024-10-05 19:03:27 +0800

    feat:(集成扫码支付所有接口，写了单元测试类)

--- src/main/java/com/gjy/wxpay/payment/nativepay/INativePayApi.java
+++ src/main/java/com/gjy/wxpay/payment/nativepay/INativePayApi.java
@@ -1,6 +1,9 @@
 package com.gjy.wxpay.payment.nativepay;
 
+import com.gjy.wxpay.payment.nativepay.model.GetRefundOrderResponse;
 import com.gjy.wxpay.payment.nativepay.model.PrepayResponse;
+import com.gjy.wxpay.payment.nativepay.model.QueryOrderByOutTradeNoResponse;
+import com.gjy.wxpay.payment.nativepay.model.RefundOrderResponse;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -21,6 +24,39 @@ public interface INativePayApi {
             @Field("body") String body,
             @Field("timestamp") String timestamp,
             @Field("notify_url") String notifyUrl,
+            @Field("sign") String sign
+    );
+
+    @FormUrlEncoded
+    @POST("api/wxpay/get_pay_order")
+    @Headers("content-type: application/x-www-form-urlencoded")
+    Call<QueryOrderByOutTradeNoResponse> getPayOrder(
+            @Field("mch_id") String mchId,
+            @Field("out_trade_no") String outTradeNo,
+            @Field("timestamp") String timestamp,
+            @Field("sign") String sign
+    );
+
+    @FormUrlEncoded
+    @POST("api/wxpay/refund_order")
+    Call<RefundOrderResponse> refundOrder(
+            @Field("mch_id") String mchId,
+            @Field("out_trade_no") String outTradeNo,
+            @Field("out_refund_no") String outRefundNo,
+            @Field("timestamp") String timestamp,
+            @Field("refund_fee") String refundFee,
+            @Field("refund_desc") String refundDesc,
+            @Field("notify_url") String notifyUrl,
+            @Field("sign") String sign
+    );
+
+    @FormUrlEncoded
+    @POST("api/wxpay/get_refund_order")
+    @Headers("content-type: application/x-www-form-urlencoded")
+    Call<GetRefundOrderResponse> getRefundOrder(
+            @Field("mch_id") String mchId,
+            @Field("out_refund_no") String outRefundNo,
+            @Field("timestamp") String timestamp,
             @Field("sign") String sign
     );
 }
--- src/test/java/com/gjy/wxpay/NativePayServiceTest.java
+++ src/test/java/com/gjy/wxpay/NativePayServiceTest.java
@@ -5,8 +5,7 @@ import com.gjy.wxpay.factory.Configuration;
 import com.gjy.wxpay.factory.PayFactory;
 import com.gjy.wxpay.factory.defaults.DefaultPayFactory;
 import com.gjy.wxpay.payment.nativepay.NativePayService;
-import com.gjy.wxpay.payment.nativepay.model.PrepayRequest;
-import com.gjy.wxpay.payment.nativepay.model.PrepayResponse;
+import com.gjy.wxpay.payment.nativepay.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
@@ -42,6 +41,42 @@ public class NativePayServiceTest {
         log.info("请求参数:{}", JSON.toJSONString(request));
         log.info("应答结果:{}", JSON.toJSONString(response));
 
+    }
+
+    @Test
+    public void test_queryOrderByOutTradeNo() throws Exception {
+        QueryOrderByOutTradeNoRequest request = new QueryOrderByOutTradeNoRequest();
+        request.setMchid("1673424392");
+        request.setOutTradeNo("31443088");
+        QueryOrderByOutTradeNoResponse response = nativePayService.queryOrderByOutTradeNo(request);
+        log.info("请求参数: {}", JSON.toJSONString(request));
+        log.info("应答结果: {}", JSON.toJSONString(response));
+    }
+
+    @Test
+    public void test_refundOrder() throws Exception {
+        RefundOrderRequest request = new RefundOrderRequest();
+        request.setMchid("1673424392");
+        request.setOutTradeNo("31443088");
+        request.setOutRefundNo(RandomStringUtils.randomNumeric(8));
+        request.setRefundFee("0.01");
+        request.setRefundDesc("测试退款");
+        request.setNotifyUrl("https://www.baidu.com/");
+
+        Object response = nativePayService.refundOrder(request);
+        log.info("请求参数: {}", JSON.toJSONString(request));
+        log.info("应答结果: {}", JSON.toJSONString(response));
+    }
+
+    @Test
+    public void test_getRefundOrder() throws Exception {
+        GetRefundOrderRequest request = new GetRefundOrderRequest();
+        request.setMchid("1673424392");
+        request.setOutRefundNo("86549206");
+
+        GetRefundOrderResponse response = nativePayService.getRefundOrder(request);
+        log.info("请求参数: {}", JSON.toJSONString(request));
+        log.info("应答结果: {}", JSON.toJSONString(response));
     }
 
 }
